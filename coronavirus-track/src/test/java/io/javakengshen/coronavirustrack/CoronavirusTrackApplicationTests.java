37f38752e6e356c1cd0331fd63d6ea6d72a0d2bd
592363789 <592363789@qq.com>
2020-11-20 17:25:37 +0800

    create a spring-boot project

---
+++ coronavirus-track/src/test/java/io/javakengshen/coronavirustrack/CoronavirusTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javakengshen.coronavirustrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
